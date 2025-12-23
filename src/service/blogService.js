5fefd4842b03b7e6bfb81eeb3b15494ae6c3bc3c
haotq2003 <khongnhoten2003@gmail.com>
2025-12-23 14:11:37 +0700

    fix

--- src/service/blogService.js
+++ src/service/blogService.js
@@ -14,13 +14,23 @@ export const BlogService = {
   }
 },
 
-  getBlogsStatusDraft: async (pageNo, pageSize,Draft) => {
+  getBlogsStatus: async (pageNo, pageSize,status) => {
     try {
-      const res = await BlogApiController.getBlogs(pageNo, pageSize, Draft);
+      const res = await BlogApiController.getBlogs(pageNo, pageSize, status);
       return res.data.result.content;
     } catch (error) {
       const message =
         error.response?.data?.message || error.message || "Get blogs failed.";
+      throw new Error(message);
+    }
+  },
+  checkBlog: async (blogId) => {
+    try {
+      const res = await BlogApiController.checkBlog(blogId);
+      return res.data.result;
+    } catch (error) {
+      const message =
+        error.response?.data?.message || error.message || "Check blog failed.";
       throw new Error(message);
     }
   },
